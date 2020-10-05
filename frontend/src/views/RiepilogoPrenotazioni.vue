a326b70fc8cf767fcfbe43a28024810c63b88984
luigigentile <luigi.gentile2@gmail.com>
2020-10-05 18:38:54 +0200

    prima installazione

--- frontend/src/views/RiepilogoPrenotazioni.vue
+++ frontend/src/views/RiepilogoPrenotazioni.vue
@@ -28,14 +28,17 @@
         </div>
 
     <!--    Elenco movimenti prenotazione    -->
-    <div class="container table-striped" v-for="(turno,index) in turni_filtrati"   :key="index">
+    <div class="container table-striped" v-for="(turno) in turni_filtrati"   :key="turno.id">
         <div class="row">
             <div class="col-lg-2 border-bottom" v-text="turno.data"> </div>
             <div class="col-lg-2 border-bottom" v-text="turno.orario_turno"> </div>
             <div class="col-lg-2 border-bottom" v-text="turno.settore"> </div>
             <div class="col-lg-1 border-bottom text-right" v-text="turno.numero_posti_disponibili"> </div>
-            <div class="col-lg-2 border-bottom text-right"> {{ getNumeroAlunniPrenotatiPerTurno(turno.id) }} </div>
-            <div class="col-lg-2 border-bottom text-right" v-text="turno.numero_posti_disponibili-postiPrenotati"> </div>
+            <div class="col-lg-1 border-bottom text-right" v-text="numeriAlunniPrenotatiPerTurno[turno.id]"> </div>
+<!--
+            <div class="col-lg-2 border-bottom text-right">  {{ getNumeroAlunniPrenotatiPerTurno(turno.id) }} </div>
+-->
+            <div class="col-lg-2 border-bottom text-right" v-text="turno.numero_posti_disponibili-numeriAlunniPrenotatiPerTurno[turno.id]"> </div>
 
         </div>
     </div>
@@ -56,12 +59,14 @@ export default {
       prenotazioni: [],
       turni:[],
       turni_filtrati:[],
+      numeriAlunniPrenotatiPerTurno:[],
       distinct_data_turni:[],
       data_turno:null,
       movimentiPrenotazioni: [],
       postiPrenotati : null,
       next: null,
       loadingPrenotazioni: false,
+      numeroTurni:null,
   };
   },
 
@@ -71,9 +76,10 @@ export default {
         var numeroTotaleAlunni
         this.postiPrenotati = 0
         numeroTotaleAlunni = 0
-//        alert("idTurno= " + idturno)
+//        alert("idTurno= " )
        for (j=0; j<this.movimentiPrenotazioni.length; j++) {
             if (this.movimentiPrenotazioni[j].turno == idturno) {
+                alert(this.movimentiPrenotazioni[j].turno)
                    numeroTotaleAlunni = numeroTotaleAlunni + this.movimentiPrenotazioni[j].numero_alunni
            }
         }
@@ -81,6 +87,48 @@ export default {
         return numeroTotaleAlunni
 
     },
+
+    getNumeroAlunniPrenotatiPerTurnoNew(idturno) {
+        var j
+        var numeroTotaleAlunni
+        this.postiPrenotati = 0
+        numeroTotaleAlunni = 0
+//        alert("sono in geto= " + idturno)
+       for (j=0; j<this.movimentiPrenotazioni.length; j++) {
+            if (this.movimentiPrenotazioni[j].turno == idturno) {
+//                    alert(this.movimentiPrenotazioni[j].turno)
+                   numeroTotaleAlunni = numeroTotaleAlunni + this.movimentiPrenotazioni[j].numero_alunni
+           }
+        }
+        this.postiPrenotati = numeroTotaleAlunni
+        return numeroTotaleAlunni
+
+    },
+
+    SetNumeroAlunniPrenotatiPerTurno() {
+        this.numeriAlunniPrenotatiPerTurno = []
+        var i,j,idturno
+
+//        alert("idTurno= " + idturno)
+//        alert("this.movimentiPrenotazioni.length" + this.movimentiPrenotazioni.length)
+        for (i=0; i<this.turni.length ;i++) {
+            idturno =this.turni[i].id
+            this.numeriAlunniPrenotatiPerTurno[idturno] = 0
+            for (j=0; j<this.movimentiPrenotazioni.length; j++) {
+//                    alert(this.movimentiPrenotazioni[j].turno)
+//                    alert(this.turni[i].id)
+                 if (this.movimentiPrenotazioni[j].turno == idturno) {
+                     this.numeriAlunniPrenotatiPerTurno[idturno] = this.numeriAlunniPrenotatiPerTurno[idturno] + this.movimentiPrenotazioni[j].numero_alunni
+                 }
+             }
+
+           }
+        //        this.postiPrenotati = numeroTotaleAlunni
+        return 0
+
+    },
+
+
 
     getPrenotazioni() {
       let endpoint = `/api/prenotazioni/`;
@@ -93,6 +141,7 @@ export default {
           let endpoint = `/api/turni/?ordering=data`;
           apiService(endpoint).then(data => {
             this.turni.push(...data.results);
+            this.numeroTurni =  this.turni.length
         });
     },
 
@@ -132,13 +181,6 @@ export default {
 
     },
 
-
-
-
-
-
-
-
     async getMovimentiPrenotazioni() {
       let endpoint = `/api/movimentiPrenotazioni/`;
       apiService(endpoint).then(data => {
@@ -157,8 +199,40 @@ export default {
     this.getTurniFiltratiPerData();
 
 
+
     //        document.getElementById('SelectUser').selectedIndex  = "1"
     document.title = "Riepilogo Prenotazioni";
-  }
+},
+    ready() {
+        alert("ready")
+    },
+
+    beforeUpdate() {
+        if (this.numeroTurni != null) {
+            this.SetNumeroAlunniPrenotatiPerTurno();
+//        alert("numero Turni" + this.numeroTurni)
+}
+    },
+
+
+
+//    updated() {
+//        alert("updated")
+//        alert("numero Turni" + this.numeroTurni)
+//    },
+
+//    mounted() {
+//            this.SetNumeroAlunniPrenotatiPerTurno();
+//            alert(this.numeroTurni)
+//        alert("mounted")
+//    },
+
+    destroyed() {
+
+//        alert("destroyed")
+    },
+
+
+
 };
 </script>
