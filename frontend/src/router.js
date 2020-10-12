f63e110e95da94bb34a87f1d5aeef2d0839f3ba6
luigigentile <luigi.gentile2@gmail.com>
2020-10-12 12:53:19 +0200

    Versione del 12-10-2020

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -9,6 +9,7 @@ import ElencoPrenotazioni from "./views/ElencoPrenotazioni.vue";
 import PrenotazioneConfirmDelete from "./views/PrenotazioneConfirmDelete.vue";
 import UserEditor from "./views/UserEditor.vue";
 import UserResetPassword from "./views/UserResetPassword.vue";
+import About from "./views/About.vue";
 
 Vue.use(VueRouter);
 
@@ -50,11 +51,15 @@ const routes = [
   {
     path: "/about",
     name: "About",
+    component: About,
+    props: false
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "./views/About.vue")
+//    component: () => import(/* webpackChunkName: "about" */ "./views/About.vue")
   },
+
+
   {
     path: "/prenotazioni/:pk/",
     name: "prenotazione-editor",
