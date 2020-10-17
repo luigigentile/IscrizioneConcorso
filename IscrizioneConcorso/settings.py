564526762d771eed2d437277953e768e224f0440
luigigentile <luigi.gentile2@gmail.com>
2020-10-17 16:32:21 +0200

    modifica About 17-10-20

--- IscrizioneConcorso/settings.py
+++ IscrizioneConcorso/settings.py
@@ -129,15 +129,13 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'it'
-
+LANGUAGE_CODE = 'it-IT'
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
+
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
