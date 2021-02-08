ef2966d33117b81073b42edc99d424c6ac026d4a
luigigentile <luigi.gentile2@gmail.com>
2021-02-08 19:18:43 +0100

    modifiche dell 08-02-21 con copia comppleta

--- frontend/src/views/ElencoPrenotazioni.vue
+++ frontend/src/views/ElencoPrenotazioni.vue
@@ -169,8 +169,8 @@ export default {
 
 
     getPrenotazioni() {
-         //let endpoint = `/api/prenotazioni/?ordering=-data_prenotazione`;
-         let endpoint = `/api/prenotazioni/`;
+         let endpoint = `/api/prenotazioni/?ordering=data_prenotazione`;
+        // let endpoint = `/api/prenotazioni/`;
          apiService(endpoint).then(data => {
         this.prenotazioni.push(...data.results);
          });
