4b93197cdf5f237868535af79ee64d13e26d7826
luigigentile <luigi.gentile2@gmail.com>
2021-03-04 10:59:50 +0100

    modifiche del 26-02-21 inserimento n.ro telefono nel modulo di iscrizione

--- users/forms.py
+++ users/forms.py
@@ -6,13 +6,16 @@ from django.contrib.auth.forms import UserCreationForm,UserChangeForm
 class CustomUserForm(RegistrationForm):
     class Meta(RegistrationForm.Meta):
         model = CustomUser
+        fields = ('username','email','telefono')
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = CustomUser
-
+        fields = ('username','email','telefono')
 
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta(UserChangeForm.Meta):
         model = CustomUser
+        fields = ('username','email','telefono')
+
