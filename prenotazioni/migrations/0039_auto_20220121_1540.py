2c77b8d0a4a516708e6a1c76f171462e8032396a
luigigentile <luigi.gentile2@gmail.com>
2022-01-21 19:46:02 +0100

    Update 0039_auto_20220121_1540.py

--- prenotazioni/migrations/0039_auto_20220121_1540.py
+++ prenotazioni/migrations/0039_auto_20220121_1540.py
@@ -9,4 +9,15 @@ class Migration(migrations.Migration):
         ('prenotazioni', '0038_auto_20220121_1100'),
     ]
 
-    
+    operations = [
+        migrations.AddField(
+            model_name='movimentiprenotazione',
+            name='orario_turno',
+            field=models.CharField(max_length=30, null=True),
+        ),
+        migrations.AddField(
+            model_name='movimentiprenotazione',
+            name='settore',
+            field=models.CharField(max_length=31, null=True),
+        ),
+    ]
